4df36abd8b4157de257b057f84baae94fd505616
AleksandrSoiko <farshchef777@gmail.com>
2022-12-11 19:29:54 +0000

    unification

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,15 +3,15 @@ import { galleryItems } from "./gallery-items.js";
 
 // Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
 const galleryContainerRef = document.querySelector(".gallery");
-createItemOfGallery(galleryItems);
+createItemOfGallery(galleryItems, galleryContainerRef);
 
-function createItemOfGallery(array) {
+function createItemOfGallery(array, elementRef) {
   const galleryItemsRef = array
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
     })
     .join("");
-  galleryContainerRef.innerHTML = galleryItemsRef;
+  elementRef.innerHTML = galleryItemsRef;
 }
 // Реалізація делегування на div.gallery і отримання url великого зображення.
 
@@ -27,7 +27,9 @@ galleryContainerRef.addEventListener("click", (event) => {
 function closeModalWindow(element) {
   const addEventFunction = (event) => {
     if (event.code === "Escape" && element.visible() === true) {
-      element.close(document.removeEventListener("keydown", addEventFunction));
+      element.close(() =>
+        document.removeEventListener("keydown", addEventFunction)
+      );
     }
   };
   document.addEventListener("keydown", addEventFunction);
